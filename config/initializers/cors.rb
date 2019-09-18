be043de8ac36b0a029f8cf05cff77d13e9632d0e
aidenjl193 <aidenjl193@gmail.com>
2019-09-18 12:20:55 +0100

    allow localhost development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -23,4 +23,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              :expose  => ['access-token', 'expiry', 'token-type', 'Authorization'],
              methods: %i(get post put patch delete options head)
   end
+    allow do
+    origins 'localhost:3000'
+    resource '*',
+             headers: :any,
+             :expose  => ['access-token', 'expiry', 'token-type', 'Authorization'],
+             methods: %i(get post put patch delete options head)
+  end
 end
